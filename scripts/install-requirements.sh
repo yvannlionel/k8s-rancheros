94a7991ec4a9bd5ae305ec0dbe0bebd537de11bc
walid <walid@walidos>
2020-03-17 12:48:52 +0100

    add nodejs wget

--- scripts/install-requirements.sh
+++ scripts/install-requirements.sh
@@ -4,6 +4,6 @@ dnf config-manager --add-repo=https://download.virtualbox.org/virtualbox/rpm/el/
 &&  dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y \
 && dnf install binutils kernel-devel kernel-headers libgomp make patch gcc glibc-headers glibc-devel dkms -y \
 && dnf install VirtualBox-6.0 -y \
-&& yum install git vim -y \
+&& yum install git vim nodejs wget -y \
 && yum install https://releases.hashicorp.com/vagrant/2.2.7/vagrant_2.2.7_x86_64.rpm -y
 
